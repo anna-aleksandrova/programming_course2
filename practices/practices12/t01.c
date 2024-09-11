6aeed381af204e6df7994949d8ec165da4f9c955
Anna Aleksandrova <anyaaleksandrova1000@gmail.com>
2024-09-11 09:50:11 +0100

    added practices 1,2

---
+++ practices/practices12/t01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int x1 = 2 + 31;
+    printf("2 + 31 = %d\n", x1);
+    int x2 = 45 * 54 - 11;
+    printf("45 * 54 - 11 = %d\n", x2);
+    int x3 = 15 / 4;
+    printf("15 / 4 = %d\n", x3);
+    float x4 = 15.0 / 4;
+    printf("15.0 / 4 = %.3f\n", x4);
+    int x5 = 67 % 5;
+    printf("67 %% 5 = %d\n", x5);
+    float x6 = (2 * 45.1 + 3.2) / 2;
+    printf("(2 * 45.1 + 3.2) / 2 = %.2f\n", x6);
+}
